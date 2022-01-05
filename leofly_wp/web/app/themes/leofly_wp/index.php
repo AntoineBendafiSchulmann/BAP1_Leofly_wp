8b4ac04c4a936e7eedf226dfd80c19f966c3d647
Nattan <nattan.bournizel@edu.devinci.fr>
2022-01-05 17:35:57 +0100

    finally finish the test and corectly done

--- leofly_wp/web/app/themes/leofly_wp/index.php
+++ leofly_wp/web/app/themes/leofly_wp/index.php
@@ -15,6 +15,19 @@
 get_header();
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.scss">
+    <title>test</title>
+</head>
+<body>
+    
+</body>
+</html>
 
 	
 
