dfb2cbb50969a082d2cd3a455437ddb4e3554e43
“Antoine <antoine.bendafi-schulmann@devinci.fr>
2022-01-05 14:32:51 +0100

    clearing plus img folder

--- leofly_wp/web/app/themes/leofly_wp/footer.php
+++ leofly_wp/web/app/themes/leofly_wp/footer.php
@@ -11,22 +11,7 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'leofly_wp' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'leofly_wp' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'leofly_wp' ), 'leofly_wp', '<a href="http://abendafi">abendafi</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	
 
 <?php wp_footer(); ?>
 
