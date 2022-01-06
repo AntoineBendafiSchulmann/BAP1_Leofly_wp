33549b5ac8a705d9852a9f9f1bc34230bc03d70d
Nattan <nattan.bournizel@edu.devinci.fr>
2022-01-06 12:11:41 +0100

    add new style and text

--- leofly_wp/web/app/themes/leofly_wp/index.php
+++ leofly_wp/web/app/themes/leofly_wp/index.php
@@ -12,6 +12,7 @@
  * @package leofly_wp
  */
 
+
 get_header();
 ?>
 
@@ -22,10 +23,28 @@ get_header();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.scss">
-    <title>test</title>
+    <title>home page</title>
+    <link rel="stylesheet" href="https://use.typekit.net/bah6hrm.css">
 </head>
 <body>
     
+    <img class="patrouille_chasseur" src="app/themes/leofly_wp/img/patrouille_de_chasseur_français.png" alt="patrouille de chasseur français dans les airs">
+    
+    <section class="premiere_description">
+
+        <img class="membre_superman" src="#" alt="membres de léofly faisant la pose">
+        <img class="decoration_site" src="#" alt="décoration verticale pour le site">
+        <div class="text_homepage">
+        <p>L'assocation</p>
+        <h1 class="titre_homepage">Leo'Fly</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis similique repudiandae soluta maxime omnis dolore, nostrum cum, magnam suscipit nisi quo ducimus obcaecati quos eum, ea ipsum ad eveniet ipsa.</p>
+        <div class="notre_histoire">
+            <p class="bouton_homepage"> Notre Histoire </p>
+        </div>
+        </div>
+
+    </section>
+
 </body>
 </html>
 
@@ -34,3 +53,5 @@ get_header();
 <?php
 
 get_footer();
+
+?>
