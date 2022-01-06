ae88eb6a1bd3864eb74a47acdb4464c6213d4c45
“Antoine <antoine.bendafi-schulmann@devinci.fr>
2022-01-06 18:11:46 +0100

    v1 of the footer and navbar

--- leofly_wp/web/app/themes/leofly_wp/footer.php
+++ leofly_wp/web/app/themes/leofly_wp/footer.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * The template for displaying the footer
@@ -10,6 +11,59 @@
  */
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.scss">
+</head>
+
+<body>
+
+<footer class="footer">
+
+<div class="logofooter">
+
+<img class="logofoot" src="app/uploads/2022/01/Logo-LeoFly-Bleu-Sans-Fond-Vectoriel.png" alt="imgfoot">
+
+</div>
+
+<div class="txtfooter">
+    <a class="links" href="#"><p class="linkfoot">Contact</p></a>
+    <a class="links" href="#"><p class="linkfoot">Partenaires</p></a>
+    <a class="links" href="#"><p class="linkfoot">L'équipe</p></a>
+</div>
+
+<div class="socialnet">
+
+<a class="socialink" href="#">
+<img  class="logo" src="app/uploads/2022/01/Icon-awesome-instagram.png" alt="imginst">
+</a>
+
+<a class="socialink" href="#">
+<img class="logo" src="app/uploads/2022/01/Icon-awesome-facebook.png" alt="imgfb">
+</a>
+
+<a class="socialink" href="#">
+<img class="logo" src="app/uploads/2022/01/Icon-awesome-linkedin-in.png" alt="imglinkdn">
+</a>
+
+<a class="socialink" href="#">
+<img class="logo" src="app/uploads/2022/01/Trace-74.png" alt="imgyt">
+</a>
+
+<a class="socialink" href="#">
+<img class="logo" src="app/uploads/2022/01/Trace-102.png" alt="imgtwitter">
+</a>
+
+
+</div>
+
+</footer>
+
 
 	
 
--- leofly_wp/web/app/themes/leofly_wp/header.php
+++ leofly_wp/web/app/themes/leofly_wp/header.php
@@ -10,12 +10,14 @@
  */
 
 ?>
-<!doctype html>
-<html <?php language_attributes(); ?>>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.scss">
+    
 
 	<?php wp_head(); ?>
 </head>
@@ -26,34 +28,40 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'leofly_wp' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$leofly_wp_description = get_bloginfo( 'description', 'display' );
-			if ( $leofly_wp_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $leofly_wp_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'leofly_wp' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
+		<nav class="navbar_menu">
+
+		<img  class="logonav" src="app/uploads/2022/01/LeoFly-blanc-fond-transparent.png" alt="imginst">
+
+		<div class="nav_menus">
+
+		
+		<a class="navlinks" href="#"><p class="linknav leftspace">Accueil</p></a>
+
+    	<a class="navlinks" href="#"><p class="linknav">Nos travaux</p></a>
+
+    	<a class="navlinks" href="#"><p class="linknav">L'équipe</p></a>
+
+		<a class="navlinks" href="#"><p class="linknav">Nous contacter</p></a>
+
+		<a class="navlinks" href="#"><p class="linknav">Partenaires</p></a>
+		
+
+		</div>
+
+		<div >
+
+		<a class="joinlink" href="#"><p class="orangetxt">Nous rejoindre</p></a>
+
+		</div>
+
+
+		<div class="languagediv">
+
+		<img  class="langchange" src="app/uploads/2022/01/Icon-ionic-ios-arrow-down.png" alt="imginst">
+		<img  class="flaglang" src="app/uploads/2022/01/france.png" alt="imginst">
+		</div>
+
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
