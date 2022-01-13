3429e3a425f93674c6b027703408dd81cfc04259
Nattan <nattan.bournizel@edu.devinci.fr>
2022-01-13 10:08:22 +0100

    bug

--- leofly_wp/web/app/themes/leofly_wp/header.php
+++ leofly_wp/web/app/themes/leofly_wp/header.php
@@ -41,7 +41,6 @@
 		<li>
 
 
-<<<<<<< HEAD
     	<a class="navlinks " href="#"><p class="linknav">Nos travaux</p></a>
 			<ul  class="sous">
 					<li class="undermenu"><a href="#">Projets</a></li>
@@ -49,9 +48,7 @@
 					<li class="undermenu"><a href="#">Articles</a></li>
 			</ul>
 		</li>
-=======
     	<a class="navlinks" href="<?php echo get_template_directory_uri()?>/single-membres-bureau.php"><p class="linknav">L'équipe</p></a>
->>>>>>> BOURNIZEL
 
 		<li>
     	<a class="navlinks" href="#"><p class="linknav">L'équipe</p></a>
