86eea9fcb5b50749656221473d321e3c107728f1
“Antoine <antoine.bendafi-schulmann@devinci.fr>
2022-01-13 17:39:53 +0100

    history page

--- leofly_wp/web/app/themes/leofly_wp/single-histoire.php
+++ leofly_wp/web/app/themes/leofly_wp/single-histoire.php
@@ -18,6 +18,63 @@
 get_header();
 ?>
 
+<section class="history-top">
+		<div class="gradient"></div>
+		<div class="anecdote">
+			<h4>ANECDOTE DU JOUR :</h4>
+			<h2><span class="span1">"</span> Ceux qui ont beacoup à espérer et rien à<br> perdre seront toujours dangeureux.<span class="span2">"</span></h2>
+		</div>
+		<img class="imganec" src="/app/uploads/2022/01/Groupe_de_masques_5.png" alt="photo-asso-groupe-pôle">
+	</section>
+
+	<section class="history-middle">
+		<h4>Notre</h4>
+		<h1>Histoire</h1>
+	</section>
+
+	<section class="history-bottom">
+		<div class="left">
+			<p class="orange">Qui sommes nous ?</p>
+			<p>Que faisons nous?</p>
+			<p>Quand l'association a-t-elle été fondé ?</p>
+			<p>Nous retrouver</p>
+			<p>Quand l'association a-t-elle été fondé ?</p>
+		</div>
+		<div class="middle">
+			<div class="point"></div>
+			<div class="barr"></div>
+			<div class="point"></div>
+		</div>
+		<div class="right">
+			<div class="description">
+				<img src="" alt="">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum praesentium quia consequuntur tenetur. Vel temporibus vero neque. Earum pariatur labore doloremque corporis nisi cupiditate ratione iusto, placeat blanditiis sint fuga itaque saepe quod totam tempora natus molestias iure maxime repellat veniam, rerum hic magnam? Qui facilis aperiam minima nisi veritatis?</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae animi, soluta nemo voluptatum magnam quam quas tempore. Blanditiis, soluta ullam eveniet reprehenderit odio, tempora harum explicabo facere provident hic autem. Molestiae sint ut doloribus! Ipsa quae quam est rem eveniet laboriosam quod tenetur, omnis quaerat officia sunt, ullam voluptatibus saepe?</p>
+			</div>
+			<div class="description display">
+				<img src="" alt="">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus molestias rerum natus rem sapiente eveniet vitae? Vero doloribus ad veniam autem! Doloribus modi ad enim suscipit esse sapiente sint corrupti, quaerat ea hic perspiciatis est vero? Porro doloremque odit quos ipsum, facere placeat laboriosam deserunt, cum, voluptatibus nisi cupiditate a.</p>
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit numquam recusandae voluptates maiores error vel deserunt ex quae fuga? Pariatur quisquam voluptatem cupiditate fugiat, dolore nihil sequi aliquid, aut ab voluptatibus doloribus quod, rem nisi vero culpa ipsum adipisci iste? Nisi, nulla suscipit autem distinctio perspiciatis voluptate quo beatae officiis?
+				</p>
+			</div>
+			<div class="description display">
+				<img src="" alt="">
+				<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur, iure voluptates accusamus, officia neque corrupti eveniet impedit rerum, ad maiores unde deleniti ut illo eligendi quae fugit laborum accusantium quis. Inventore voluptate, quisquam laboriosam reiciendis odio, necessitatibus aliquid minus nihil nam nesciunt eaque reprehenderit ab ad at! Itaque, voluptatibus delectus?</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus rem, tempore tenetur, quaerat quia mollitia, saepe praesentium exercitationem necessitatibus a atque ad itaque modi distinctio debitis dolore error. Totam quam illum et blanditiis tempora placeat repellat laudantium! Dolorem eum porro, incidunt possimus explicabo autem maxime nisi dolorum, esse iusto ullam.</p>
+			</div>
+			<div class="description display">
+				<img src="" alt="">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi consequuntur atque recusandae, harum, sapiente animi perferendis reiciendis numquam dicta quos esse, eveniet optio earum! Iste fuga mollitia illum. Quaerat adipisci impedit eius ipsa minima molestiae, architecto nobis numquam praesentium sunt. Aperiam cum animi aliquam eaque nobis? Cum maiores recusandae quis!</p>
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, architecto a. Tenetur id soluta suscipit voluptate perferendis autem molestiae ipsam porro, in voluptatum. Ab, iusto necessitatibus. Unde rerum libero ullam, labore repudiandae doloremque sapiente vero adipisci molestias sunt magni odio doloribus dignissimos consequuntur, nam nemo quo laudantium maiores! Recusandae, itaque.</p>
+			</div>
+			<div class="description display">
+				<img src="" alt="">
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident libero at debitis unde nihil quaerat magnam accusamus corporis, laboriosam officiis consequatur enim ipsa ab voluptatum iusto numquam totam incidunt molestiae voluptas blanditiis earum ad repellendus, sint quae! Error blanditiis necessitatibus repudiandae pariatur! Eum et ut accusantium sit quidem laudantium doloribus.</p>
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam molestias itaque minus eligendi sit et, temporibus nobis dolorum aut, sed dolor veritatis iure quis id odit eveniet officiis enim laudantium, doloremque debitis perferendis quisquam. Est doloremque, labore nostrum, omnis provident optio at vel sint nam tempore adipisci repellat enim nulla.</p>
+			</div>
+
+		</div>
+	</section>
 
 	
 
